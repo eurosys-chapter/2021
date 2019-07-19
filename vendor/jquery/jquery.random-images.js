1c1dc8c8e20467bf326a7496d325f748ce4cd8f2
Pramod Bhatotia <pramod.bhatotia@gmail.com>
2019-07-20 01:49:00 +0200

    first commit -- logo

--- vendor/jquery/jquery.random-images.js
+++ vendor/jquery/jquery.random-images.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
 var images=['img/001.jpg',
             'img/002.jpg',
             'img/003.jpg',
-            'img/004.jpg',
-            'img/005.jpg',];
+            'img/004.jpg'];
 
 var randomNumber = Math.floor(Math.random() * images.length);
 var bgImg = 'url(' + images[randomNumber] + ')';
